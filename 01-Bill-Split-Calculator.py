4929ace28b2cca1f6f8b6e9c716588efb2e19d87
Jasmeen-Kaur0077 <someoneoneone07@gmail.com>
2026-08-01 12:00:20 +0530

    Create 01-Bill-Split-Calculator.py
    Add Bill Split Calculator project

---
+++ 01-Bill-Split-Calculator.py
@@ -0,0 +1,13 @@
+print("Bill Split Calculator")
+
+bill_amount = float(input("bill_amount: "))
+tip_percentage = float(input("tip_percentage: "))
+
+tip_amount = tip_percentage / 100 * bill_amount
+total = bill_amount + tip_amount
+print(f"Total: ${total:.2f}")
+
+number_of_person = int(input('No. of person: '))
+
+split_amount = total / number_of_person
+print(f"each will pay: ${split_amount:.2f}")
